44dc7b87c7cd29c2a4a6af647839f81f8afcac1f
dev-tr26 <trangadiarudra26@gmail.com>
2026-03-10 14:35:29 +0530

    fixed-model-paths

--- backend/loader.py
+++ backend/loader.py
@@ -2,29 +2,32 @@ import torch
 from pathlib import Path
 import gdown
 
-MODEL_DIR = Path("models")
-MODEL_PATH = Path("../model_1/music_genre_model_1.pt")
+DOWNLOAD_MODEL_PATH = Path("models/music_genre_model_1.pt")
+LOCAL_MODEL_PATH = Path("../model_1/music_genre_model_1.pt")
 
 
 FILE_ID = "10sUE7cKe7pk9Knj4Smymv16CbTxKZgKJ"
 
 def download_model():
-    MODEL_DIR.mkdir(exist_ok=True)
+    DOWNLOAD_MODEL_PATH.parent.mkdir(parents=True,exist_ok=True)
 
     url = f"https://drive.google.com/file/d/{FILE_ID}/view?usp=drive_link"
 
     print("Downloading model from Google Drive...")
-    gdown.download(url, str(MODEL_PATH), quiet=False)
+    gdown.download(url, str(DOWNLOAD_MODEL_PATH), quiet=False)
 
 
 
 def load_model():
     
-    if not MODEL_PATH.exists():
-        download_model()
+    if LOCAL_MODEL_PATH.exists():
+        model_path = LOCAL_MODEL_PATH
+    else:
+        if not DOWNLOAD_MODEL_PATH.exists():
+            download_model()
+        model_path = DOWNLOAD_MODEL_PATH
         
-    
-    model = torch.jit.load(MODEL_PATH, map_location="cpu")
+    model = torch.jit.load(model_path, map_location="cpu")
     model = torch.jit.optimize_for_inference(model)
     
     model.eval()
